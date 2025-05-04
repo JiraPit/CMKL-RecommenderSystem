b9dec1af9001c08baa331445f18876974c628b5f
Sarutch Supaibulipat (Uni) <112068235+POKerPONGus@users.noreply.github.com>
2025-05-04 13:32:45 +0700

    fix: incorrect file paths

--- system/evaluation/run_eval.py
+++ system/evaluation/run_eval.py
@@ -4,6 +4,17 @@ import re
 
 PY_INTERPRETER = "python3"
 
+def is_output_outdated(srcs, output):
+    if not os.path.exists(output):
+        return True
+    
+    src_modify_times = list(map(os.path.getmtime, srcs))
+    output_modify_time = os.path.getmtime(output)
+
+    if all([output_modify_time > src_time for src_time in src_modify_times]):
+        return False
+    return True
+
 def run_processing_scripts(scripts, datasets):
     def get_output_path(file):
         search_res = re.search("^generate_(.+?).py$", os.path.basename(file))
@@ -12,7 +23,12 @@ def run_processing_scripts(scripts, datasets):
     
     outputs = [get_output_path(script) for script in processing_scripts]
     for script, dataset, output in zip(scripts, datasets, outputs):
-        if os.system(f"{PY_INTERPRETER} {script} {dataset} {output}") != 0:
+        if not is_output_outdated([script, dataset], output):
+            print(f"{output} is already up to date")
+            continue
+
+        status = os.system(f"{PY_INTERPRETER} {script} {dataset} {output}")
+        if status != 0:
             print(f"{script} failed to run")
             sys.exit(1)
     return outputs
@@ -24,21 +40,30 @@ def run_comparsion_script(reference: str, prediction: str):
         pred_base = os.path.basename(pred).removesuffix('.csv')
         return os.path.join(dir, f"{ref_base}_vs_{pred_base}.csv")
 
-    compare_script = "compare_recommendations.py"
+    dir = os.path.dirname(reference)
+    script = os.path.join(dir, "compare_recommendations.py")
     output = get_output_path(reference, prediction)
-    if os.system(f"{PY_INTERPRETER} {compare_script} {reference} {prediction} {output}") != 0:
-        print(f"{compare_script} failed to run")
+
+    if not is_output_outdated([script, reference, prediction], output):
+        print(f"{output} is already up to date")
+        return output
+
+    status = os.system(f"{PY_INTERPRETER} {script} {reference} {prediction} {output}")
+    if status != 0:
+        print(f"{script} failed to run")
         sys.exit(1)
     return output
 
 def run_calculation_script(comparision_result):
-    calc_script = "calculate_mean_overlap.py"
-    if os.system(f"{PY_INTERPRETER} {calc_script} {comparision_result}") != 0:
-        print(f"{calc_script} failed to run")
+    dir = os.path.dirname(comparision_result)
+    script = os.path.join(dir, "calculate_mean_overlap.py")
+    status = os.system(f"{PY_INTERPRETER} {script} {comparision_result}")
+    if status != 0:
+        print(f"{script} failed to run")
         sys.exit(1)
 
 # check cli argument
-get_search_str = lambda s: f"^generate_{s}_from_.+?\.py$"
+get_search_str = lambda s: f"^.*\/?generate_{s}_from_.+?\.py$"
 script_types = ["ref", "pred"]
 if len(sys.argv) != 3:
     print(f"{__file__} <generate_ref_file> <generate_pred_file>")
@@ -52,11 +77,12 @@ for file, type in zip(sys.argv[1:], script_types):
         sys.exit(1)
 
 dir = os.path.dirname(sys.argv[0])
-processing_scripts = [os.path.join(dir, script) for script in sys.argv[1:]]
+cwd = os.path.join(dir, os.pardir)
+os.chdir(cwd)
+eval_dir = os.path.basename(dir)
+processing_scripts = [os.path.join(eval_dir, os.path.basename(path)) for path in sys.argv[1:]]
 
-# print(os.getcwd(), __file__, dir)
-
-dataset_dir = os.path.join(os.pardir, "datasets", "original", "articles")
+dataset_dir = os.path.join("datasets", "original", "articles")
 dataset_filenames = ["user-item-interactions.csv", "articles_community.csv"]
 dataset_paths = [os.path.join(dataset_dir, f) for f in dataset_filenames]
 
@@ -69,7 +95,3 @@ for path in dataset_paths:
 res = run_processing_scripts(processing_scripts, dataset_paths)
 res = run_comparsion_script(*res)
 run_calculation_script(res)
-
-
-
-
